1d8d854f4f09d5e69cbc4c919ddff06b5134522a
Hyeseong-Myeong <a01025819869@gmail.com>
2024-03-01 14:44:39 +0900

    fix BoardRepository Query

--- src/main/java/com/sch/crane/cranewebbackend_v2/Data/Repository/Board/BoardRepository.java
+++ src/main/java/com/sch/crane/cranewebbackend_v2/Data/Repository/Board/BoardRepository.java
@@ -5,6 +5,7 @@ import com.sch.crane.cranewebbackend_v2.Domain.Entity.User;
 import com.sch.crane.cranewebbackend_v2.Domain.Enums.BoardCategory;
 import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -13,15 +14,16 @@ import java.util.Optional;
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
     @Override
-    @Query("select b from Board b where b.bid =:bid")
+    @Query("select b from Board b where b.bid = :bid")
     Optional<Board> findById(@Param("bid") Long bid);
 
-    @Query("select b from Board b where b.boardCategory =: boardCategory")
+    @Query("select b from Board b where b.boardCategory =:boardCategory")
     List<Board> findBoardByCategory(@Param("boardCategory") BoardCategory boardCategory);
 
     @Query("select b from Board b where b.user =: user")
     List<Board> findBoardByUser(@Param("user") User user);
 
-    @Query("update Board b set b.boardView = b.boardView +1 where b.bid =: bid")
+    @Modifying
+    @Query("update Board b set b.boardView = b.boardView +1 where b.bid = :bid")
     int increaseView(@Param("bid") Long bid);
 }
